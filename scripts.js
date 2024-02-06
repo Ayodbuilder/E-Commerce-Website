61288ef4621d2198642adb7da68985039c6e156f
unknown <olayinkavictor68@gmail.com>
2024-02-06 12:50:26 +0100

    more updates

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('Navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active')}
+    )}   
+     {
+    
+}
