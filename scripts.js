1ab16da028abbcbb2dfc69cf6c897a1aa4158649
unknown <olayinkavictor68@gmail.com>
2024-02-06 17:00:12 +0100

    Updated

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
 const bar = document.getElementById('bar');
+const close = document.getElementById('close');
 const nav = document.getElementById('Navbar');
+
 
 if (bar) {
     bar.addEventListener('click', () => {
-        nav.classList.add('active')}
-    )}   
-     {
-    
+        nav.classList.add('active');
+    })
 }
+
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+     
+    
+    
